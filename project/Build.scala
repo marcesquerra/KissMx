aeeb92fdbf221174a8577a5af113e1588a3d67dd
Marc Esquerrà i Bayo <esquerra@w3d.cc>
2014-02-04 23:04:06 +0000

    Empty Skeleton

---
+++ project/Build.scala
@@ -0,0 +1,62 @@
+import sbt._
+import Keys._
+
+object BuildSettings
+{
+
+	val buildSettings = Defaults.defaultSettings ++ Seq(
+			organization := "com.bryghts.kissmx",
+			version      := "0.0.1",
+			scalaVersion := "2.10.3",
+			scalacOptions ++= Seq(),
+			libraryDependencies += "org.scala-lang" % "scala-reflect" % "2.10.2",
+			libraryDependencies += "org.specs2" %% "specs2" % "2.2.2" % "test",
+			libraryDependencies += "org.scalacheck" %% "scalacheck" % "1.10.1" % "test",
+			libraryDependencies += "junit" % "junit" % "4.11" % "test",
+			resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/",
+			scalacOptions in Test ++= Seq("-Yrangepos"),
+			publishMavenStyle := true,
+			publishTo <<= version { (v: String) =>
+				val nexus = "https://oss.sonatype.org/"
+				if (v.trim.endsWith("SNAPSHOT")) Some("snapshots" at nexus + "content/repositories/snapshots") 
+				else                             Some("releases"  at nexus + "service/local/staging/deploy/maven2")
+			},
+			publishArtifact in Test := false,
+//			useGpg := true,
+			pomExtra := (
+				<url>http://www.brights.com</url>
+				<licenses>
+					<license>
+					<name>mit</name>
+					</license>
+				</licenses>
+				<scm>
+					<url>git@github.com:marcesquerra/KissMx.git</url>
+					<connection>scm:git:git@github.com:marcesquerra/KissMx.git</connection>
+				</scm>
+				<developers>
+					<developer>
+						<name>Marc Esquerrà i Bayo</name>
+						<email>esquerra@bryghts.com</email>
+					</developer>
+				</developers>
+			),
+			pomIncludeRepository := { _ => false }
+	)
+
+}
+
+object MyBuild extends Build
+{
+	val projectName = "KissMx"
+
+	import BuildSettings._
+
+
+	lazy val root: Project = Project(
+		projectName,
+		file("."),
+		settings = buildSettings
+	)
+
+}
